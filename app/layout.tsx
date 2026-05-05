2ecaee047d07b57cdc13c0c0903b8c1599059cfa
status1999 <status007@outlook.com>
2026-05-05 10:46:18 +0200

    Add AdSense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9217263434665129"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
